84d31c2ee86649abc0b492063a69c1e96573639c
helany <teacherhelany@gmail.com>
2022-10-20 21:55:28 -0300

    adicionando ex de operadores

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -14,41 +14,60 @@ console.log(negar2)
 
 console.log("*************************************************************************")
 
-const salario = 2000
-const creche = 45.50
-const filho = 2
 
-const novoSalarioComAuxilio = (filho * creche) + salario
-console.log("Novo salário com aumento do auxilio da creche R$", novoSalarioComAuxilio)
-console.log("*************************************************************************")
-// Janeiro: R$ 5.784,50
+
+// - Janeiro: R$ 5.784,50
 // - Fevereiro: R$ 3.418,41
 // - Março: R$ 4.124,10
 // - Abril: R$ 1.874,00
 // - Maio: R$ 7.000,00
 // - Junho: R$ 9.450,00
-
-
-
-const novoSalarioJan = 5784.50 * 0.1 + novoSalarioComAuxilio
-
-const novoSalarioFev = (3418.41 * 10) / 100 + novoSalarioComAuxilio 
-
-const novoSalarioMar = (4124.10 * 10) / 100 + novoSalarioComAuxilio
-
-const novoSalarioAbril = (1.874,00 * 10) / 100 + novoSalarioComAuxilio
-
-console.log("Salario com comissão em Janeiro R$",novoSalarioJan)
-console.log("Salario com comissão em Feveiro R$",novoSalarioFev)
-console.log("Salario com comissão em Março R$",novoSalarioMar)
-console.log("Salario com comissão em Abril R$",novoSalarioAbril)
-
+const salario = 2000
+const auxPorFilho = 45.50
+const qtdFilhos = 2
+//
+const vendasJaneiro = 5784.50
+const vendasFevereiro = 3418.41
+const vendasMarco = 4124.10
+const vendasAbril = 1874
+const vendasMaio = 7000
+const vendasJunho = 9450
+const salarioComAuxilio = salario + (qtdFilhos * auxPorFilho)
+console.log("Salário com auxilio creche R$", salarioComAuxilio)
 console.log("*****************************************************")
+const salarioJan = salarioComAuxilio + (vendasJaneiro * 0.1)
+const salarioFev = salarioComAuxilio + (vendasFevereiro * 0.1)
+const salarioMarco = salarioComAuxilio + (vendasMarco * 0.1)
+const salarioAbril = salarioComAuxilio + (vendasAbril * 0.1)
+const salarioMaio = salarioComAuxilio + (vendasMaio * 0.1)
+const salarioJunho = salarioComAuxilio + (vendasJunho * 0.1)
+console.log("*****************************************************")
+console.log("Salário de Jan com comissão de 10%", salarioJan)
+console.log("Salário de Fev com comissão de 10%", salarioFev)
+console.log("Salário de Março com comissão de 10%", salarioMarco)
+console.log("Salário de Abril com comissão de 10%", salarioAbril)
+console.log("Salário de Maio com comissão de 10%", salarioMaio)
+console.log("Salário de Junho com comissão de 10%", salarioJunho.toFixed(2))
+console.log("*****************************************************")
+console.log("*****************************************************")
+const salarioComDescJan = salarioJan  - (salarioJan * 0.05)
+const salarioComDescFev = salarioFev  - (salarioFev *  0.05)
+const salarioComDescMarco = salarioMarco -  (salarioMarco *  0.05)
+const salarioComDescAbril = salarioAbril -  (salarioAbril*  0.05)
+const salarioComDescMaio = salarioMaio  - (salarioMaio *  0.05)
+const salarioComDescJunho = salarioJunho -  (salarioJunho *  0.05)
+console.log("Salário de Janeiro com desconto do INSS de 5%", salarioComDescJan)
+console.log("Salário de Fevereiro com desconto do INSS de 5%", salarioComDescFev)
+console.log("Salário de Março com desconto do INSS de 5%", salarioComDescMarco)
+console.log("Salário de Abril com desconto do INSS de 5%", salarioComDescAbril)
+console.log("Salário de Maio com desconto do INSS de 5%", salarioComDescMaio)
+console.log("Salário de Junho com desconto do INSS de 5%", salarioComDescJunho)
 
-const salarioComDescontoJ = novoSalarioJan - (novoSalarioJan * (5 / 100))
-const salarioComDescontoF = novoSalarioFev - (novoSalarioFev * 0.05)
-console.log("Salario com desconto do INSS em Janeiro R$", salarioComDescontoJ.toFixed(2))
-console.log("Salario com desconto do INSS em Janeiro R$", salarioComDescontoF.toFixed(2))
+const mediaDeSalario = (salarioJan + salarioFev + salarioMarco + salarioAbril + salarioMaio + salarioJunho ) / 6
+console.log("*****************************************************")
+console.log("*****************************************************")
+console.log("A média de salario em 6 meses é de", mediaDeSalario)
+
 
 
 
